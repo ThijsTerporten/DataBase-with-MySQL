0c493d48497228053637322a8ac874c4d856a188
ThijsTerporten <twpterporten@gmail.com>
2021-11-11 21:56:56 +0000

    Final commit

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,23 +1,20 @@
 import os
+import datetime
 import pymysql
 
-# Get username from cloud9 workspace
-# Modify this if running on another environment
-
+# Get the username from the Cloud9 workspace
+# (modify this variable if running on another environment)
 username = os.getenv('C9_USER')
 
-# Connect to database
-connection = pymysql.connect(
-    host='localhost',
-    user=username,
-    password='',
-    db='Chinook')
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user=username,
+                             password='',
+                             db='Chinook')
 
 try:
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        cursor.execute("UPDATE Friends SET age = 22 WHERE name = 'bob';")
+        connection.commit()
 finally:
     connection.close()
